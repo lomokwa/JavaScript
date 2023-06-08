a8623116d00ff04a5f36caa5e8694bfa20ee7681
Lorenzo Mokwa <lomokwa@Lorenzos-MacBook-Air.local>
2023-06-08 10:42:52 -0400

    defined object myCar

--- arrays-and-objects/objects.js
+++ arrays-and-objects/objects.js
@@ -6,3 +6,13 @@ const student = {
 }
 
 console.log(student.firstName);
+
+// Creating objects from separate variables
+
+const make = "BMW"
+const chassi = "E90"
+const model = "325i"
+const year = 2006
+const engine = "N52"
+
+const myCar = {make, model, year, chassi}
