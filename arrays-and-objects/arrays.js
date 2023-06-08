506de9ba14b9e073ec802df4b47d21953e531d53
Lorenzo Mokwa <lomokwa@Lorenzos-MacBook-Air.local>
2023-06-08 11:55:05 -0400

    testing .push and .unshift

--- arrays-and-objects/arrays.js
+++ arrays-and-objects/arrays.js
@@ -2,3 +2,8 @@ const students = ["Sam", "Cihan", "Ariana", "Yaslin", "Kevin"]
 
 console.log(students[2]); // Ariana
 console.log(students.length); // 5
+
+students.push("Lorenzo");
+students.unshift("Andrew");
+
+console.log(students); // Andrew, Cihan, Ariana, Yaslin, Kevin, Lorenzo
