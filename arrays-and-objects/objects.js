26efdb531759364b9d375681486e1647ed1ca089
Lorenzo Mokwa <lomokwa@Lorenzos-MacBook-Air.local>
2023-06-08 11:52:09 -0400

    added movie object

--- arrays-and-objects/objects.js
+++ arrays-and-objects/objects.js
@@ -3,16 +3,37 @@ const student = {
   lastName: "Mokwa",
   age: 19,
   student: true,
-}
+};
 
 console.log(student.firstName);
 
 // Creating objects from separate variables
 
-const make = "BMW"
-const chassi = "E90"
-const model = "325i"
-const year = 2006
-const engine = "N52"
+const make = "BMW";
+const chassi = "E90";
+const model = "325i";
+//const year = 2006;
+const engine = "N52";
 
-const myCar = {make, model, year, chassi}
+//const myCar = {make, model, year, chassi}; // Assembled 3 variables into a single object
+
+// Destructuring objects
+
+const movie = {
+  title: "Drive",
+  year: 2011,
+  director: "Nicolas W.",
+  genre: ["action","drama"]
+};
+
+// Lame way:
+
+// const title = movie.title;
+// const year = movie.year;
+// const director = movie.director;
+// const genre = movie.genre;
+
+// Cool way:
+
+const {title, writer, genre} = movie;
+console.log(movie);
