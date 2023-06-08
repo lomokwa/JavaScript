4c507142f2b9c98424c6adc00c456dfe7f542410
Lorenzo Mokwa <lomokwa@Lorenzos-MacBook-Air.local>
2023-06-08 10:30:50 -0400

    added arrays.js and objects.js

---
+++ arrays-and-objects/arrays.js
@@ -0,0 +1,4 @@
+const students = ["Sam", "Cihan", "Ariana", "Yaslin", "Kevin"]
+
+console.log(students[2]); // Ariana
+console.log(students.length); // 5
---
+++ arrays-and-objects/objects.js
@@ -0,0 +1,8 @@
+const student = {
+  firstName: "Lorenzo",
+  lastName: "Mokwa",
+  age: 19,
+  student: true,
+}
+
+console.log(student.firstName);
